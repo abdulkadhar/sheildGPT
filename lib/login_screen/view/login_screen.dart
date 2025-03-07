0d806cf08def1c783fce4b38c9d29b17d907d1ab
Abdulkadhar <s.abdulkadhar11@gmail.com>
2025-03-08 00:45:57 +0530

    86cy708r2 refinement

--- lib/login_screen/view/login_screen.dart
+++ lib/login_screen/view/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shield_gpt/login_screen/widgets/ripple_container.dart';
-import 'package:simple_ripple_animation/simple_ripple_animation.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
