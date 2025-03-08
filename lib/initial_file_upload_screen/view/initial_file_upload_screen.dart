c877252b3f1acf5ac02775691f7156904a41be24
Abdulkadhar <s.abdulkadhar11@gmail.com>
2025-03-09 05:21:21 +0530

    Addition of state management

--- lib/initial_file_upload_screen/view/initial_file_upload_screen.dart
+++ lib/initial_file_upload_screen/view/initial_file_upload_screen.dart
@@ -2,7 +2,9 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 import 'package:shield_gpt/global_widgets/SGPT_button.dart';
+import 'package:shield_gpt/initial_file_upload_screen/controller/initial_file_upload_screen_controller.dart';
 
 class InitialFileUploadScreen extends StatelessWidget {
   const InitialFileUploadScreen({super.key});
@@ -11,80 +13,95 @@ class InitialFileUploadScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Center(
-            child: DottedBorder(
-              dashPattern: const [15, 5],
-              color: Colors.white,
-              borderType: BorderType.RRect,
-              radius: const Radius.circular(12),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.45,
-                width: MediaQuery.of(context).size.height * 0.45,
-                decoration: BoxDecoration(
-                  color: const Color(0XFF202020),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Lottie.asset(
-                      'assets/lottie/file_upload_animation.json',
-                      height: 200,
-                      width: 200,
-                    ),
-                    const SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        "Please do upload the PCAP file to get analyzed",
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.quicksand(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14,
+      body: Consumer<InitialFileUploadScreenController>(builder: (
+        context,
+        initialFileUploadScreenController,
+        child,
+      ) {
+        return Stack(
+          children: [
+            Center(
+              child: DottedBorder(
+                dashPattern: const [15, 5],
+                color: Colors.white,
+                borderType: BorderType.RRect,
+                radius: const Radius.circular(12),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.45,
+                  width: MediaQuery.of(context).size.height * 0.45,
+                  decoration: BoxDecoration(
+                    color: const Color(0XFF202020),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Lottie.asset(
+                        'assets/lottie/file_upload_animation.json',
+                        height: 200,
+                        width: 200,
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          "Please do upload the PCAP file to get analyzed",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.quicksand(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14,
+                          ),
                         ),
                       ),
+                      const SizedBox(height: 20),
+                      SGPTButton(
+                        label: 'Analyze',
+                        onPress: () {
+                          // NOTE toggling the value of the status
+                          initialFileUploadScreenController.setUploadingStatus(
+                            !initialFileUploadScreenController
+                                .getUploadingStatus(),
+                          );
+                        },
+                        isLoading: initialFileUploadScreenController
+                            .getUploadingStatus(),
+                        loadingLabel: 'Uploading the file',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // SECTION - Logo Section
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      'assets/images/ripple_logo_white.png',
+                      height: 40,
+                      width: 60,
                     ),
-                    const SizedBox(height: 20),
-                    SGPTButton(
-                      label: 'Analyze',
-                      onPress: () {},
+                    Text(
+                      "Shield GPT",
+                      style: GoogleFonts.quicksand(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w200,
+                      ),
                     )
                   ],
                 ),
               ),
-            ),
-          ),
-          // SECTION - Logo Section
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    'assets/images/ripple_logo_white.png',
-                    height: 40,
-                    width: 60,
-                  ),
-                  Text(
-                    "Shield GPT",
-                    style: GoogleFonts.quicksand(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w200,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+            )
+          ],
+        );
+      }),
     );
   }
 }
