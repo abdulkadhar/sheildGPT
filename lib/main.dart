df8afb92f478ffec7c4cf4a673e7466e46ab4a51
Abdulkadhar <s.abdulkadhar11@gmail.com>
2025-03-08 16:44:24 +0530

    86cy72cqh Addition of provider support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shield_gpt/global_controller/app_scroll_behaviour.dart';
+import 'package:shield_gpt/login_screen/controller/login_screen_controller.dart';
 import 'package:shield_gpt/login_screen/view/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LoginScreenController()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
