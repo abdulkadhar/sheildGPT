1d6b4787518f7ddda58cd080304054f523982eb1
Abdulkadhar <s.abdulkadhar11@gmail.com>
2025-03-08 16:44:01 +0530

    86cy72cqh Addition of form validation

--- lib/login_screen/view/login_screen.dart
+++ lib/login_screen/view/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:shield_gpt/global_widgets/SGPT_Text_Form_Field.dart';
+import 'package:shield_gpt/login_screen/controller/login_screen_controller.dart';
 import 'package:shield_gpt/login_screen/widgets/ripple_container.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -25,86 +27,102 @@ class _LoginScreenState extends State<LoginScreen> {
                 pageSnapping: false,
                 children: [
                   // SECTION - Login Screen Section
-                  Center(
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "SIGN IN",
-                            style: GoogleFonts.quicksand(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.w100,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 50,
-                          ),
-                          SgptTextFormField(
-                            labelText: 'Email',
-                            onChanged: (emailValue) {},
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          SgptTextFormField(
-                            labelText: 'Password',
-                            isPassword: true,
-                            onChanged: (passwordValue) {},
-                          ),
-                          const SizedBox(
-                            height: 80,
-                          ),
-                          ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 50,
-                                vertical: 20,
-                              ),
-                              shape: const RoundedRectangleBorder(
-                                side: BorderSide(
-                                  color: Colors.white,
-                                  width: 0.3,
-                                ),
-                              ),
-                            ),
-                            onPressed: () {},
-                            child: Text(
+                  Consumer<LoginScreenController>(
+                      builder: (context, loginScreenController, child) {
+                    return Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
                               "SIGN IN",
                               style: GoogleFonts.quicksand(
                                 color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w100,
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 60,
-                          ),
-                          InkWell(
-                            onTap: () {
-                              controller.nextPage(
-                                duration: const Duration(seconds: 1),
-                                curve: Curves.ease,
-                              );
-                            },
-                            child: Text(
-                              'CREATE ACCOUNT',
-                              style: GoogleFonts.quicksand(
-                                color: Colors.white,
-                                fontSize: 10,
-                                fontWeight: FontWeight.w200,
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            SgptTextFormField(
+                              labelText: 'Email',
+                              onChanged: (emailValue) {
+                                loginScreenController.setUserName(emailValue);
+                              },
+                              errorText:
+                                  loginScreenController.userNameErrorText,
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            SgptTextFormField(
+                              labelText: 'Password',
+                              isPassword: true,
+                              onChanged: (passwordValue) {
+                                print('ON_CHANGED: $passwordValue');
+                                loginScreenController
+                                    .setPassword(passwordValue);
+                              },
+                              errorText:
+                                  loginScreenController.passwordErrorText,
+                            ),
+                            const SizedBox(
+                              height: 80,
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 50,
+                                  vertical: 20,
+                                ),
+                                shape: const RoundedRectangleBorder(
+                                  side: BorderSide(
+                                    color: Colors.white,
+                                    width: 0.3,
+                                  ),
+                                ),
+                              ),
+                              onPressed: () {
+                                print(
+                                    'username value: ${loginScreenController.getUserName()}\nPassword Value: ${loginScreenController.getUsePassword()}');
+                              },
+                              child: Text(
+                                "SIGN IN",
+                                style: GoogleFonts.quicksand(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                            const SizedBox(
+                              height: 60,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                controller.nextPage(
+                                  duration: const Duration(seconds: 1),
+                                  curve: Curves.ease,
+                                );
+                              },
+                              child: Text(
+                                'CREATE ACCOUNT',
+                                style: GoogleFonts.quicksand(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w200,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  }),
                   // SECTION Sign up form
                   Center(
                     child: SizedBox(
