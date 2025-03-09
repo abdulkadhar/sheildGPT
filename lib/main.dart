01fe6367cbd8df62c51ac9468233427c88e1b19f
Abdulkadhar <s.abdulkadhar11@gmail.com>
2025-03-10 01:45:04 +0530

    routing change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shield_gpt/dashboard_landing_screen/view/dashboard_landing_screen.dart';
 import 'package:shield_gpt/global_controller/app_scroll_behaviour.dart';
 import 'package:shield_gpt/initial_file_upload_screen/controller/initial_file_upload_screen_controller.dart';
 import 'package:shield_gpt/loader_screen/view/loader_screen.dart';
@@ -30,7 +31,7 @@ class ShieldGPTApp extends StatelessWidget {
       title: 'Shield GPT',
       // TODO uncomment this after the refinement
       // home: const LoginScreen(),
-      home: const LoaderScreen(),
+      home: const DashboardLandingScreen(),
     );
   }
 }
